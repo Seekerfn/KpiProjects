33289d16aeb4f6e46570b5abf726dd2eaac0e1bd
Zarytskyi Oleksandr <pfantom180@gmail.com>
2025-09-16 22:10:34 +0300

    a little change in code

--- lesson 1/Task1.java
+++ lesson 1/Task1.java
@@ -6,8 +6,7 @@ public class Task1 {
         double c = 10.874;
         double d = -2.91;
 
-        double y = (Math.log(Math.abs(2 * a)) / (3 * Math.tan(b)))
-                - Math.sqrt(c / Math.sqrt(Math.abs(d)));
+        double y = (Math.log(Math.abs(2 * a)) / (3 * Math.tan(b))) - Math.sqrt(c / Math.sqrt(Math.abs(d)));
 
         System.out.println("y = " + y);
     }
